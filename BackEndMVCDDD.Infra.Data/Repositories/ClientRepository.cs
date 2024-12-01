53901334649acbcf8f30fc7658fcd07a9c39a9eb
gabriella-guimaraes <gabigara13@gmail.com>
2024-12-01 17:27:45 -0300

    feat: Specialized Client Repository setup

---
+++ BackEndMVCDDD.Infra.Data/Repositories/ClientRepository.cs
@@ -0,0 +1,11 @@
+﻿
+using BackEndMVCDDD.Domain.Entities;
+using BackEndMVCDDD.Domain.Interfaces;
+
+namespace BackEndMVCDDD.Infra.Data.Repositories
+{
+    public class ClientRepository : RepositoryBase<Client>, IClientRepository //Repositório especializado
+    {
+        //Usar para buscar mais específicas
+    }
+}
