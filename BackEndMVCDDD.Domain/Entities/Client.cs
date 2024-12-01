ec6746b0006dfeb45c8f3de1b2430ab2f94897c2
gabriella-guimaraes <gabigara13@gmail.com>
2024-12-01 12:02:12 -0300

    feat: Client class entity

---
+++ BackEndMVCDDD.Domain/Entities/Client.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BackEndMVCDDD.Domain.Entities
+{
+    public class Client
+    {
+        public int ClientId { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime SubscriptionDate { get; set; }
+        public bool IsActive { get; set; }
+
+        //Regra de Negócio
+        public bool SpecialClient(Client client)
+        {
+            return client.IsActive && DateTime.Now.Year - client.SubscriptionDate.Year >= 5;
+        }
+
+    }
+}
