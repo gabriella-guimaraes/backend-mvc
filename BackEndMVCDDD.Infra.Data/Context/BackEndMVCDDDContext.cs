058ff792a85302b4294f087da823f2c8ac975514
gabriella-guimaraes <gabigara13@gmail.com>
2024-12-01 14:36:54 -0300

    feat: Db model config

--- BackEndMVCDDD.Infra.Data/Context/BackEndMVCDDDContext.cs
+++ BackEndMVCDDD.Infra.Data/Context/BackEndMVCDDDContext.cs
@@ -1,5 +1,9 @@
 ﻿using BackEndMVCDDD.Domain.Entities;
+using System;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace BackEndMVCDDD.Infra.Data.Context
 {
@@ -12,5 +16,51 @@ namespace BackEndMVCDDD.Infra.Data.Context
         }
 
         public DbSet<Client> Clients { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Remove pré configuração que pluraliza os nomes das tabelas ao serem 
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            // Remove pré configuração de exclusão em cascada (um para muitos)
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+
+            // Remove pré configuração de exclusão em cascada (muitos para muitos)
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            // Toda vez que tivermos uma entidade, o Contexto entederá que Nome + Id será uma primaryKey
+            modelBuilder.Properties()
+                .Where(prop => prop.Name == prop.ReflectedType.Name + "Id")
+                .Configure(prop => prop.IsKey());
+
+            // Padronização varchar para strings
+            modelBuilder.Properties<string>()
+                .Configure(prop => prop.HasColumnType("varchar"));
+
+            // Padronização de tamanho para strings
+            modelBuilder.Properties<string>()
+                .Configure(prop => prop.HasMaxLength(100));
+
+        }
+        // Pré configuração para o campo de DateTime no momento de mudanças
+        public override int SaveChanges()
+        {
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("SubscriptionDate") != null))
+            {
+                // Adicionando um cadastro
+                if(entry.State == EntityState.Added)
+                {
+                    entry.Property("SubscriptionDate").CurrentValue = DateTime.Now;
+                }
+
+                // Modificando um cadastro
+                if(entry.State == EntityState.Modified)
+                {
+                    entry.Property("SubscriptionDate").IsModified = false;
+                }
+            
+            }
+            return base.SaveChanges();
+        }
     }
 }
