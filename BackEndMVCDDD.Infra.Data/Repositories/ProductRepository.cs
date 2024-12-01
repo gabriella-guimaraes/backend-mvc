c9df304cf45bdd9cdc117c66e46c61158796dba0
gabriella-guimaraes <gabigara13@gmail.com>
2024-12-01 17:34:48 -0300

    feat: Specialized Product Repository setup

---
+++ BackEndMVCDDD.Infra.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using BackEndMVCDDD.Domain.Entities;
+using BackEndMVCDDD.Domain.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BackEndMVCDDD.Infra.Data.Repositories
+{
+    public class ProductRepository : RepositoryBase<Product>, IProductRepository
+    {
+        public IEnumerable<Product> SearchByName(string name)
+        {
+            //Busca especializada
+            return Db.Products.Where(p => p.Name == name);
+        }
+    }
+}
