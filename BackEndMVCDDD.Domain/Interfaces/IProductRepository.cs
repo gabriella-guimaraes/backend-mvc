dcb4a121d394fa6701dc272b84d00fb126bafd07
gabriella-guimaraes <gabigara13@gmail.com>
2024-12-01 17:30:07 -0300

    feat: Product Interface created

---
+++ BackEndMVCDDD.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿
+using BackEndMVCDDD.Domain.Entities;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BackEndMVCDDD.Domain.Interfaces
+{
+    public interface IProductRepository : IRepositoryBase<Product>
+    {
+        IEnumerable<Product> SearchByName(string name);
+    }
+}
