5f884ea5a423d44f3f05ca09d42225cbecdc339e
gabriella-guimaraes <gabigara13@gmail.com>
2024-12-17 21:55:49 -0300

    feat: IClientService interface created

---
+++ BackEndMVCDDD.Domain/Interfaces/Services/IClientService.cs
@@ -0,0 +1,13 @@
+﻿using BackEndMVCDDD.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEndMVCDDD.Domain.Interfaces.Services
+{
+    public interface IClientService : IServiceBase<Client>
+    {
+    }
+}
