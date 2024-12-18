51cff9e1acdec95e45961aaefd817383f324e480
gabriella-guimaraes <gabigara13@gmail.com>
2024-12-17 21:57:52 -0300

    feat: IProductService interface created

---
+++ BackEndMVCDDD.Domain/Interfaces/Services/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using BackEndMVCDDD.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEndMVCDDD.Domain.Interfaces.Services
+{
+    public interface IProductService : IServiceBase<Product>
+    {
+        IEnumerable<Product> SearchByName(string name);
+    }
+}
