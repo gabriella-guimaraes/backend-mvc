4783d345e015a83b03f9f5bac8b60044f1a92f41
gabriella-guimaraes <gabigara13@gmail.com>
2024-12-17 22:07:24 -0300

    feat: ServiceBase created

---
+++ BackEndMVCDDD.Domain/Services/ServiceBase.cs
@@ -0,0 +1,49 @@
+﻿using BackEndMVCDDD.Domain.Interfaces.Repositories;
+using BackEndMVCDDD.Domain.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEndMVCDDD.Domain.Services
+{
+    public class ServiceBase<TEntity> : IDisposable, IServiceBase<TEntity> where TEntity : class
+    {
+        private readonly IRepositoryBase<TEntity> _repositoryBase;
+
+        public ServiceBase(IRepositoryBase<TEntity> repositoryBase)
+        {
+            _repositoryBase = repositoryBase;
+        }
+        public void Add(TEntity obj)
+        {
+            _repositoryBase.Add(obj);
+        }
+
+        public void Dispose()
+        {
+            _repositoryBase.Dispose();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            _repositoryBase.GetAll();
+        }
+
+        public TEntity GetById(int id)
+        {
+            _repositoryBase.GetById(id);
+        }
+
+        public void Remove(TEntity obj)
+        {
+            _repositoryBase.Remove(obj);
+        }
+
+        public void Update(TEntity obj)
+        {
+            _repositoryBase.Update(obj);
+        }
+    }
+}
