7e26dc5abfb564d9af392732abc0248b6b25305d
gabriella-guimaraes <gabigara13@gmail.com>
2024-12-17 22:18:19 -0300

    feat: ProductService created

---
+++ BackEndMVCDDD.Domain/Services/ProductService.cs
@@ -0,0 +1,28 @@
+﻿using BackEndMVCDDD.Domain.Entities;
+using BackEndMVCDDD.Domain.Interfaces.Repositories;
+using BackEndMVCDDD.Domain.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEndMVCDDD.Domain.Services
+{
+    public class ProductService : ServiceBase<Product>, IProductService
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductService(IProductRepository productRepository)
+            : base(productRepository)
+        {
+            _productRepository = productRepository;
+            
+        }
+
+        public IEnumerable<Product> SearchByName(string name)
+        {
+            return _productRepository.SearchByName(name);
+        }
+    }
+}
