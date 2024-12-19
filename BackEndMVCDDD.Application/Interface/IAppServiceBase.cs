a886f5fca47cc6c21768179e9da67b6002843155
gabriella-guimaraes <gabigara13@gmail.com>
2024-12-18 22:36:30 -0300

    feat: Application layer generic interface created

---
+++ BackEndMVCDDD.Application/Interface/IAppServiceBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEndMVCDDD.Application.Interface
+{
+    public interface IAppServiceBase<TEntity> where TEntity : class
+    {
+        //CRUD genérica
+        void ADD(TEntity obj);
+        TEntity GetById(int id);
+        IEnumerable<TEntity> GetAll();
+        void Update(TEntity obj);
+        void Remove(TEntity obj);
+        void Dispose();
+    }
+}
