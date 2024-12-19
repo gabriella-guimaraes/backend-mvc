883cbb11f42aebfd50175af0d162a80a79897dd2
gabriella-guimaraes <gabigara13@gmail.com>
2024-12-18 22:40:59 -0300

    feat: Application layer - product interface created

---
+++ BackEndMVCDDD.Application/Interface/IProductAppService.cs
@@ -0,0 +1,14 @@
+﻿using BackEndMVCDDD.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEndMVCDDD.Application.Interface
+{
+    public interface IProductAppService : IAppServiceBase<Product>
+    {
+        IEnumerable<Product> SearchByName(string name);
+    }
+}
